1c00433b28b036da392f9aad05187f72b79020f5
jfmachado <jonathan.fmachado01@gmail.com>
2017-10-06 17:02:18 -0300

    Final changes.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Road Trip App #
+
+Source code for Udacity Project: Neighborhood Map.
+
+Single-page application featuring a map of a road trip around Austria.
+
+### Run the project ###
+* Clone this repository
+* Open the Index.html file
+
+### Screenshot ###
+![Alt text](https://raw.githubusercontent.com/jonathanfmachado/udacity-frontend-project/master/thumbnail.png)
+
+### 3rd APIs ###
+
+This app uses the following third-party APIs to provide additional information about the locations:
+* Google Maps API
+* Flicker API
--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ var AppViewModel = function () {
         }
     });
 
+    // If filterLocations changes, the markers displayed should also change
     this.filterLocations.subscribe(function (filter_array) {
         for (var i = 0; i < markers.length; i++) {
             markers[0];
@@ -58,10 +59,10 @@ var stringStartsWith = function (string, startsWith) {
     return string.substring(0, startsWith.length).toLowerCase() === startsWith.toLowerCase();
 };
 
-var selected_marker;
+var selected_marker; // Global var
+
 function selectMarker(element) {
     var self = this;
-
     for (var i = 0; i < markers.length; i++) {
         if (markers[i].title == element.text) {
             google.maps.event.trigger(markers[i], 'click');
@@ -115,10 +116,8 @@ function addMarkers(loc_array) {
 
 };
 
-
 app_view = new AppViewModel()
 ko.applyBindings(app_view);
-
 
 
 $(document).ready(function () {
@@ -128,26 +127,38 @@ $(document).ready(function () {
 });
 
 
-
 function getFlickerImages(marker) {
+    // Request photos to the Flicker API
+
+    // The api_key and format have been hardcoded:
     url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6dcfd1ef636ddcf410d67ebfa3acefe1&per_page=3&nojsoncallback=1&format=json&text=' + marker.title;
-    $.ajax({
+
+    $.ajax({    //By default, all requests are sent asynchronously
         url: url, success: function (result) {
-            image_url_array = []
             img_string = ''
             $.each(result.photos.photo, function(){
                 url = 'https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.id+'_'+this.secret+'_s.jpg';
                 img_string+= '<img src="'+url+'" width="42" height="42">';
             });
-            
+            // Creates the InfoWindow for the clicked marker
             var infowindow = new google.maps.InfoWindow({
                 content: '<h5>' + marker.title + '</h5>' + img_string + '<p>Images by <a href="https://www.flickr.com"> Flicker</p>'
             });
+            // Add animation to the clicked marker
             marker.setAnimation(google.maps.Animation.BOUNCE);
             setTimeout(function () {
                 marker.setAnimation(null);
             }, 750); // maps duration of one bounce
             infowindow.open(map, marker);
+        },
+        // Data requests that fail are handled gracefully using common fallback techniques (i.e. AJAX error or fail methods)
+        error: function (jqXHR, textStatus, errorThrown){
+            //A function to be called if the request fails.
+            console.log(textStatus, errorThrown)
+            msg = '<div class="alert alert-danger alert-dismissable">'+
+            '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+
+            '<strong>Something went wrong :( </strong> Error: '+ textStatus +'</div>'
+            $('#feedback-messages').append(msg)
 
         }
     });
